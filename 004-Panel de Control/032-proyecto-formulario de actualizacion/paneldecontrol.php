1aeae7f308d79e7215efb8e7a0e4d680e3a5ba7f
Sergiodimu <sergio_010699@hotmail.com>
2024-01-28 21:11:42 +0100

    Ultima clase PHP

---
+++ 004-Panel de Control/032-proyecto-formulario de actualizacion/paneldecontrol.php
@@ -0,0 +1,70 @@
+<?php 
+    session_start();
+    include "log.php";
+    include "config.php";
+    include "inc/funciones.php";
+    if(isset($_GET['accion'])){
+        switch($_GET['accion']){
+            case "insertar":
+                    insertarRegistro($conexion);
+                break;
+            case "eliminar":
+                    eliminarRegistro($conexion);
+                    break;
+        }
+    }
+?>
+
+<!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Panel de Control</title>
+        <link rel="stylesheet" href="css/panel.css">
+    </head>
+    <body>
+        <?php comprobarAcceso(); ?>
+    <header>
+        <h1>Panel de Control</h1>
+    </header>
+    <main>
+        <nav>
+        <ul>
+        <?php menunavegacion($conexion); ?>
+        </ul>
+    </nav>
+        <section>
+            <a href="?operacion=nuevo&tabla=<?php echo $_GET['tabla'] ?>" class="boton nuevo">+</a>
+            <h3>Editando la tabla: <?php echo $_GET['tabla'] ?></h3>
+            <div id="contenedor">
+                <?php
+                    if(!isset($_GET['operacion'])){
+                ?>
+                    <table>
+                        <thead>
+                            <tr>
+                                <?php mostrarcolumnas($conexion); ?>
+                            </tr>
+                        </thead>
+                        <tbody>
+                                <?php mostrardatos($conexion); ?>
+                        </tbody>
+                    </table>
+                <?php
+                    }else{
+                        switch($_GET['operacion']){
+                            case "nuevo":
+                                formularioInsertar($conexion);
+                                break;
+                            case "actualizar":
+                                formularioActualizar($conexion);
+                                break;
+                        }
+                    }
+                ?>
+            </div>
+        </section>
+    </main>
+    </body>
+
+</html>
--- 005-API/nuevapregunta.php
+++ 005-API/nuevapregunta.php
@@ -2,6 +2,6 @@
     include "config.php";
 
     echo "estoy creando la pregunta"; 
-    $peticion = "INSERT INTO preguntas VALUES(NULL,2,'".$_GET['titulo']."','".$_GET['bloquetexto']."','".$_GET['palabrasclave']."',1,'".date("Y-m-d")."')";
+    $peticion = "INSERT INTO preguntas (valores) VALUES(NULL,2,'".$_GET['titulo']."','".$_GET['bloquetexto']."','".$_GET['palabrasclave']."',1,'".date("Y-m-d")."')";
     mysqli_query($conexion, $peticion);
 ?>
